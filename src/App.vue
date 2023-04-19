1a87b61301938a3c154fcb0019af0190e4d64f4c
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-19 16:24:53 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
+  <copy-right />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import { shuffled } from "./utils/array.js";
+import ResultScreen from "./components/ResultScreen.vue";
+import CopyRight from "./components/CopyRightScreen.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRight,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+
+      this.settings.cardsContext = shuffled(
+        shuffled(shuffled(shuffled(cards)))
+      );
+
+      this.settings.startedAt = new Date().getTime();
+
+      //data ready
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      //get timer
+      this.timer = new Date().getTime() - this.settings.startedAt;
+      //switch to result component
+      this.statusMatch = "result";
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/InteractScreen.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="screen">
+    <div
+      class="screen__inner"
+      :style="{
+        width: `${
+          ((((920 - 16 * 4) / Math.sqrt(cardsContext.length) - 16) * 3) / 4 +
+            16) *
+          Math.sqrt(cardsContext.length)
+        }px`,
+      }"
+    >
+      <card-flip
+        v-for="(card, index) in cardsContext"
+        :key="index"
+        :imgBackFaceUrl="`images/${card}.png`"
+        :card="{ index, value: card }"
+        :ref="`card-${index}`"
+        :cardsContext="cardsContext"
+        @onFlip="checkRule($event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardFlip from "./CardScreen.vue";
+
+export default {
+  props: {
+    cardsContext: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  components: {
+    CardFlip,
+  },
+  data() {
+    return {
+      rules: [],
+    };
+  },
+  methods: {
+    checkRule(card) {
+      if (this.rules.length === 2) return false;
+      this.rules.push(card);
+
+      if (
+        this.rules.length === 2 &&
+        this.rules[0].value === this.rules[1].value
+      ) {
+        //add class 'disabled' to component card
+        this.$refs[`card-${this.rules[0].index}`][0].onEnabledDisableMode();
+        this.$refs[`card-${this.rules[1].index}`][0].onEnabledDisableMode();
+
+        //reset rules to []
+        this.rules = [];
+        const disabledElements = document.querySelectorAll(
+          ".screen .card.disabled"
+        );
+
+        if (
+          disabledElements &&
+          disabledElements.length === this.cardsContext.length - 2
+        ) {
+          setTimeout(() => {
+            this.$emit("onFinish");
+          }, 920);
+        }
+      } else if (
+        this.rules.length === 2 &&
+        this.rules[0].value != this.rules[1].value
+      ) {
+        setTimeout(() => {
+          //close two card
+          this.$refs[`card-${this.rules[0].index}`][0].onFlipBackCard();
+          this.$refs[`card-${this.rules[1].index}`][0].onFlipBackCard();
+
+          //reset rules to []
+          this.rules = [];
+        }, 800);
+      } else return false;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.screen {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background-color: var(--dark);
+  color: var(--light);
+}
+
+.screen__inner {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2rem auto;
+}
+</style>
